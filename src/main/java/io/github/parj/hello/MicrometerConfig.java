88018322f5fa422985b932509fa18214684717e5
Parjanya Mudunuri <1268219+parj@users.noreply.github.com>
2023-08-26 14:04:58 +0000

    Added otel collector

--- src/main/java/io/github/parj/hello/MicrometerConfig.java
+++ src/main/java/io/github/parj/hello/MicrometerConfig.java
@@ -1,23 +1,23 @@
-package io.github.parj.hello;
+// package io.github.parj.hello;
 
-//FROM https://github.com/izeye/sample-micrometer-spring-boot/blob/micrometer-gh-1431/src/main/java/com/izeye/sample/config/MicrometerConfig.java
+// //FROM https://github.com/izeye/sample-micrometer-spring-boot/blob/micrometer-gh-1431/src/main/java/com/izeye/sample/config/MicrometerConfig.java
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
 
-import io.micrometer.core.instrument.logging.LoggingMeterRegistry;
+// import io.micrometer.core.instrument.logging.LoggingMeterRegistry;
 
-/**
- * Configuration for Micrometer.
- *
- * @author Johnny Lim
- */
-@Configuration
-public class MicrometerConfig {
+// /**
+//  * Configuration for Micrometer.
+//  *
+//  * @author Johnny Lim
+//  */
+// @Configuration
+// public class MicrometerConfig {
 
-    @Bean
-    public LoggingMeterRegistry loggingMeterRegistry() {
-        return new LoggingMeterRegistry();
-    }
+//     @Bean
+//     public LoggingMeterRegistry loggingMeterRegistry() {
+//         return new LoggingMeterRegistry();
+//     }
 
-}
+// }
